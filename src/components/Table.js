a65b653ed430019bdfeff75b04c2c0e175072d3f
Abhishek Verma <avishekv444@gmail.com>
2021-10-23 22:01:14 +0530

    fetching data from API

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,24 @@
+import { useState, useEffect } from 'react';
+
+import { api } from '../helpers/urls'
+
 
 
 function Table(){
+
+    const [users,setUsers]=useState([]);
+
+    useEffect(()=>{
+    fetch(api).
+    then((res)=>res.json())
+    .then((data)=>{
+        console.log("api",data);
+        setUsers(data);
+        console.log('users',users)
+    })
+    },[])
+
+    console.log("users",users)
     return(
         <div>Table</div>
     );
