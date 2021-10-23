120a57438139d8510dd0200d38ad138095cfa3fc
Abhishek Verma <avishekv444@gmail.com>
2021-10-23 23:53:50 +0530

    showing data in tables and implemented delete functionality

--- src/components/Table.js
+++ src/components/Table.js
@@ -18,9 +18,39 @@ function Table(){
     })
     },[])
 
-    console.log("users",users)
+    const deleteUser=(id)=>{
+        const filteredArray=users.filter(user=>user.id != id);
+        setUsers(filteredArray);
+    }
     return(
-        <div>Table</div>
+        
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { users.map((user)=>{
+                        return(
+                        <tr>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.role}</td>
+                            <td>
+                                <img src="https://cdn-icons-png.flaticon.com/512/1828/1828843.png" onClick={()=>deleteUser(user.id)} alt="delete" />
+                                <img src="https://cdn-icons-png.flaticon.com/512/3597/3597075.png" alt="edit" />
+                            </td>
+                        </tr>
+                        )
+                    })}
+               </tbody>
+            </table>
+        </div>
     );
 }
 
