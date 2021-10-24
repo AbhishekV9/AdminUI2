71973c0e60d4a06fd6ad5d10d0b9de9ba5f2d3dd
Abhishek Verma <avishekv444@gmail.com>
2021-10-24 17:39:34 +0530

    applied pagination

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,28 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect} from 'react';
+import _ from "lodash";
 
 import { api } from '../helpers/urls'
 import TableRow from './TableRow';
 
 
+const pageSize=10;
 
 function Table(){
-
     const [users,setUsers]=useState([]);
-    
+
+    const [paginatedUsers,setPaginatedUsers]=useState([]); 
+    const [currentPage,setCurrentPage]=useState(1);
 
     useEffect(()=>{
     fetch(api).
     then((res)=>res.json())
     .then((data)=>{
-        console.log("api",data);
         setUsers(data);
-        console.log('users',users)
+        setPaginatedUsers(_(data).slice(0).take(pageSize).value())
     })
     },[])
 
     const deleteUser=(id)=>{
-        const filteredArray=users.filter(user=>user.id !== id);
-        setUsers(filteredArray);
+        const filteredArray=paginatedUsers.filter(user=>user.id !== id);
+        const filteredArray2=users.filter(user=>user.id !== id);
+        setUsers(filteredArray2);
+        setPaginatedUsers(filteredArray);
     }
 
     const changeUserDetail=(id,newName,newEmail,newRole)=>{
@@ -48,17 +52,29 @@ function Table(){
                 .includes(newValue)
             });
             setUsers(filteredUsers);
+            setPaginatedUsers(_(filteredUsers).slice(0).take(pageSize).value())
         })
         
     }
     
+    const pageCount= users ? Math.ceil(users.length/pageSize) : 0 ; 
+    
 
+    const pages= _.range(1,pageCount+1);
+
+    const pagination=(pageNo)=>{
+        setCurrentPage(pageNo);
+        const startIndex=(pageNo-1) * pageSize ;
+        const paginatedUser= _(users).slice(startIndex).take(pageSize).value();
+        setPaginatedUsers(paginatedUser);
+    }
+    
     return(
         
-        <div>
-            <input placeholder="Search By name or email or role" onChange={filterUsers}/>
-            <table>
-                <thead>
+        <div id="main">
+            <input id="searchbar" placeholder="Search By name or email or role" onChange={filterUsers}/>
+            <table className="table">
+                <thead >
                     <tr>
                         <th>Name</th>
                         <th>Email</th>
@@ -67,13 +83,30 @@ function Table(){
                     </tr>
                 </thead>
                 <tbody>
-                    { users.map((user)=>{
+                    { paginatedUsers.map((user)=>{
                         return(
                            <TableRow user={user} deleteUser={deleteUser} changeUserDetail={changeUserDetail} key={user.id}/>
                         )
                     })}
                </tbody>
             </table>
+            <nav className="d-flex justify-content-center">
+                <ul className="pagination">
+                    {
+                        pages.map((page)=>{
+                            return <li className={
+                                page===currentPage ? "page-item active" : "page-item"
+                            }
+                            > 
+                            <p className="page-link" onClick={()=>pagination(page)}>
+                                {page}
+                            </p>
+
+                            </li>
+                        })
+                    }                  
+                </ul>
+            </nav>
         </div>
     );
 }
